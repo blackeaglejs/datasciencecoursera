9dda96b70c37ab52308d51e40267624e0e698ff7
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-07-26 01:45:46 -0400

    Cleaned up the import of the data and have the data set up to be modified further.

--- getcleandata/courseproject/run_analysis.R
+++ getcleandata/courseproject/run_analysis.R
@@ -3,10 +3,15 @@
 x_test <- "X_test.txt"
 x_test_data <- read.table(x_test, sep = "")
 
-y_test <- "y_test.csv"
+y_test <- "y_test.txt"
 y_test_data <- read.table(y_test, sep = "")
 
 test_data <- cbind(x_test_data,y_test_data)
+
+subject_test <- "subject_test.txt"
+subject_test_data <- read.table(subject_test, sep = "")
+
+test_data <- cbind(test_data, subject_test_data)
 
 ## Import the training data
 x_train <- "X_train.txt"
@@ -17,5 +22,25 @@ y_train_data <- read.table(y_train, sep = "")
 
 train_data <- cbind(x_train_data,y_train_data)
 
+subject_train <- "subject_train.txt"
+subject_train_data <- read.table(subject_train, sep = "")
+
+train_data <- cbind(train_data, subject_train_data)
+
 ## Merge The test and training data together. 
-rbind(test_data,train_data)
+all_data <- rbind(test_data,train_data)
+
+## Pull in the variable names from the data.
+colnamesfile <- read.table("features.txt")
+colnames <- colnamesfile$V2
+colnames(all_data) <- colnames
+names(all_data)[562] <- "activity"
+names(all_data)[563] <- "subject"
+
+## Replace the activity codes with their actual names. 
+all_data$activity <- ifelse(all_data$activity ==1, "walking", all_data$activity)
+all_data$activity <- ifelse(all_data$activity ==2, "walking upstairs", all_data$activity)
+all_data$activity <- ifelse(all_data$activity ==3, "walking downstairs", all_data$activity)
+all_data$activity <- ifelse(all_data$activity ==4, "sitting", all_data$activity)
+all_data$activity <- ifelse(all_data$activity ==5, "standing", all_data$activity)
+all_data$activity <- ifelse(all_data$activity ==6, "laying", all_data$activity)
